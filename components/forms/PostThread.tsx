ccbbb7c4ce9a11953c70dd475b518d1f9dfd32c9
Abhi <abhi.nolyfe@gmail.com>
2023-08-24 07:43:16 +0530

    modified for creating orgranization

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,6 +17,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 
 interface Props {
   user: {
@@ -33,6 +34,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -46,7 +48,7 @@ function PostThread({ userId }: { userId: string }) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
